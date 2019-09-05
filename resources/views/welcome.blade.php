f57e58e90bcecc5c63c8e30befe5f5822ea2d6af
Hafidz Anggit <hafydzelba@gmail.com>
2019-09-06 02:26:58 +0700

    update halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Selamat datang Pamong Desa
                 </div>
 
                 <div class="links">
