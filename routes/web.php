e51b21b86983335e26fe208847c1a9d8646e7bdc
Hafidz Anggit <hafydzelba@gmail.com>
2019-09-06 10:04:30 +0700

    tambah file view

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('halo', function () {
+	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
